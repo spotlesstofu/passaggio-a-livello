351421e97f850a852e44c3c9c8f00a6ac62d2ed0
Camilla Conte <spotlesstofu@gmail.com>
2020-10-27 19:16:11 +0100

    Add estimated time delta

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def get_reaction(wait_time):
 def get_orari_passaggio():
     orari_passaggio = []
     status = f"{get_reaction(0)} Aperto"
-    for stazione_partenza, stazione_arrivo in [(terni, marmore), (marmore, terni)]:
+    for stazione_partenza, stazione_arrivo, time_delta in [(terni, marmore, 4), (marmore, terni, 9)]:
         soluzioniViaggio = requests.get(f'{viaggiatreno}/soluzioniViaggioNew/{stazione_partenza}/{stazione_arrivo}/{get_datetime()}').json()
         for s in soluzioniViaggio["soluzioni"]:
             if s["vehicles"][0]["categoriaDescrizione"] == "Regionale":
@@ -35,7 +35,7 @@ def get_orari_passaggio():
                 # if orarioPartenza - datetime.datetime.now() < datetime.timedelta(minutes=60):
                 orarioArrivo = datetime.datetime.fromisoformat(s["vehicles"][0]["orarioArrivo"])
                 orario_chiude = orarioPartenza
-                orario_apre = orarioArrivo
+                orario_apre = orarioArrivo - datetime.timedelta(minutes=time_delta)
                 if orario_chiude <= datetime.datetime.now() < orario_apre:
                     eta = int((orario_apre - datetime.datetime.now()).seconds/60)
                     status = f"{get_reaction(eta)} Chiuso - apre tra {eta} minuti"
